b3e9fb7915c7ecc73a4e843251ae6f80ab94d2a9
Viktor <viktopiniaginya@gmail.com>
2023-03-21 20:11:45 +0300

    1. Add network manager

---
+++ e-Locator/Sources/NetworkService.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkService.swift
+//  e-Locator
+//
+//  Created by Виктор on 21.03.2023.
+//
+
+import Foundation
+
+class NetworkService {
+    private let session: URLSession
+    
+    // By using a default argument (in this case .shared) we can add dependency
+    // injection without making our app code more complicated.
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func loadData<T: Codable>(from url: URL,
+                  completionHandler: @escaping (Result<[T], Error>) -> Void) {
+        let task = session.dataTask(with: url) { data, _, error in
+            do {
+                guard let data = data else { return }
+                let convertedData = try JSONDecoder().decode([T].self, from: data)
+                completionHandler(.success(convertedData))
+            } catch {
+                completionHandler(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+}
