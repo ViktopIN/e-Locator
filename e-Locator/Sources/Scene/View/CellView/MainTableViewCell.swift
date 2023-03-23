8f641b26f979b5418ffe6100702445e17d3c84ed
Viktor <viktopiniaginya@gmail.com>
2023-03-23 20:18:50 +0300

    1. Convert TableViewCell to separate view

--- e-Locator/Sources/Scene/View/CellView/MainTableViewCell.swift
+++ e-Locator/Sources/Scene/View/CellView/MainTableViewCell.swift
@@ -6,64 +6,15 @@
 //
 
 import UIKit
-import SnapKit
 
-class MainTableViewCell: UITableViewCell {
+final class MainTableViewCell: UITableViewCell {
     
     static let reuseIdentifier = "MainTableViewCell"
     
     // MARK: - Views
     
-    private lazy var mainStackView = UIStackView.configureStackView(with: .horizontal, spacing: 20)
+    private lazy var cellContentView = UserInfoCellView()
     
-    private lazy var avatarImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFill
-        imageView.layer.masksToBounds = true
-        return imageView
-    }()
-    
-    private lazy var labelStack = UIStackView.configureStackView(with: .vertical)
-    
-    private lazy var nameLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = .systemFont(ofSize: 16)
-        label.textColor = .black
-        return label
-    }()
-    
-    private lazy var distanceLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = .systemFont(ofSize: 14)
-        label.textColor = .lightGray
-        return label
-    }()
-    
-    private lazy var favoriteMarkButton: UIButton = {
-        let button = UIButton()
-        button.setImage(
-            UIImage(systemName: "heart")?.withTintColor(.black, renderingMode: .alwaysOriginal),
-            for: .normal
-        )
-        button.setImage(UIImage(
-            systemName: "heart.fill")?.withTintColor(.red, renderingMode: .alwaysOriginal),
-            for: .selected
-        )
-        button.backgroundColor = .white
-        button.imageView?.contentMode = .center
-        button.layer.masksToBounds = false
-        button.layer.shadowColor = UIColor.lightGray.cgColor
-        button.layer.shadowOpacity = 0.3
-        button.layer.shadowRadius = 5
-        button.layer.shadowOffset = .zero
-        button.translatesAutoresizingMaskIntoConstraints = false
-                
-        return button
-    }()
-
     // MARK: - Initializer
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -71,51 +22,25 @@ class MainTableViewCell: UITableViewCell {
         
         setupHierarchy()
         setupLayout()
-        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        DispatchQueue.main.async { [unowned self] in
-            favoriteMarkButton.layer.cornerRadius = favoriteMarkButton.bounds.height / 2
-            avatarImageView.layer.cornerRadius = avatarImageView.frame.height / 2
-        }
-    }
 
     // MARK: - Settings
     
     private func setupHierarchy() {
-        addSubview(mainStackView)
-        mainStackView.addArrangedSubviews(avatarImageView, labelStack, favoriteMarkButton)
-        labelStack.addArrangedSubviews(nameLabel, distanceLabel)
+        addSubview(cellContentView)
     }
     
     private func setupLayout() {
-        mainStackView.snp.makeConstraints { make in
-            make.top.bottom.equalToSuperview().inset(10)
-            make.leading.trailing.equalToSuperview().inset(20)
-        }
-        avatarImageView.snp.makeConstraints { make in
-            make.width.equalTo(avatarImageView.snp.height)
-        }
-        favoriteMarkButton.snp.makeConstraints { make in
-            make.width.equalTo(favoriteMarkButton.snp.height)
-        }
-    }
-    
-    private func setupView() {
-        backgroundColor = .white
+        cellContentView.fillSuperview()
     }
     
     // MARK: - Methods
     
     func configurationCell(with data: UserDistanceModel) {
-        avatarImageView.image = data.image
-        nameLabel.text = data.name
-        distanceLabel.text = data.distanceDescription
+        cellContentView.configurationCell(with: data)
     }
 }
